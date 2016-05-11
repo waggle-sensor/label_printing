4c56b2f340c24fcd8d78ae306d8d6ae47ec13f64
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-05-11 16:25:22 -0500

    use new udev rule

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+# udev rules
+cp 75-Dymo-LabelWriter-450.rules /etc/udev/rules.d
+
+udevadm control --reload-rules
+
 apt-get update
 # install cups
 apt-get install -y libcups2-dev libcupsimage2-dev g++ cups cups-client
@@ -12,7 +17,7 @@ apt-get install -y printer-driver-dymo
 
 
 # add printer to cups
-/usr/sbin/lpadmin -p DymoLW450 -E -v parallel:/dev/usb/lp0 -m dymo:0/cups/model/lw450.ppd
+/usr/sbin/lpadmin -p DymoLW450 -E -v parallel:/dev/dymo_lw_450 -m dymo:0/cups/model/lw450.ppd
 
 # set default printer
 lpoptions -d DymoLW450
