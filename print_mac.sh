52b830407537366556d5a0ee3c5f7781e7913bea
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-02 15:53:15 -0500

    added 3rd line

--- print_mac.sh
+++ print_mac.sh
@@ -60,14 +60,20 @@ TEXT_X=${VIEWBOX_SIZE}
 
 LINE_1=$(echo "(${VIEWBOX_SIZE}/3)*0.9" | bc -l)
 LINE_2=$(echo "(${VIEWBOX_SIZE}/3)*1.6" | bc -l)
+LINE_3=$(echo "(${VIEWBOX_SIZE}/3)*2.3" | bc -l)
 echo "LINE_1: ${LINE_1}"
 echo "LINE_2: ${LINE_2}"
+echo "LINE_3: ${LINE_3}"
+
+if [ "${1}x" != "x" ] ; then
+    
+fi
 
 set -x
 
 sed -e 's_<svg width="[0-9]*.[0-9]*cm" height="[0-9]*.[0-9]*cm"_<svg width="2.2cm" height="1.1cm"_' \
 -e "s@viewBox=\"0 0 [0-9]* [0-9]*\"@viewBox=\"0 0 ${VIEWBOX_SIZE_DOUBLE} ${VIEWBOX_SIZE}\"@" \
--e "s$</svg>$<text x=\"${TEXT_X}\" y=\"${LINE_1}\" style=\"font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:8px;line-height:50%;font-family:'Courier New';-inkscape-font-specification:'Courier New Bold';text-align:start;writing-mode:lr-tb;text-anchor:start\" >${MAC_STRING:0:6}</text><text x=\"${TEXT_X}\" y=\"${LINE_2}\" style=\"font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:8px;line-height:50%;font-family:'Courier New';-inkscape-font-specification:'Courier New Bold';text-align:start;writing-mode:lr-tb;text-anchor:start\" >${MAC_STRING:6}</text> </svg>$" \
+-e "s$</svg>$<text x=\"${TEXT_X}\" y=\"${LINE_1}\" style=\"font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:8px;line-height:50%;font-family:'Courier New';-inkscape-font-specification:'Courier New Bold';text-align:start;writing-mode:lr-tb;text-anchor:start\" >${MAC_STRING:0:6}</text><text x=\"${TEXT_X}\" y=\"${LINE_2}\" style=\"font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:8px;line-height:50%;font-family:'Courier New';-inkscape-font-specification:'Courier New Bold';text-align:start;writing-mode:lr-tb;text-anchor:start\" >${MAC_STRING:6}</text><text x=\"${TEXT_X}\" y=\"${LINE_3}\" style=\"font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:8px;line-height:50%;font-family:'Courier New';-inkscape-font-specification:'Courier New Bold';text-align:start;writing-mode:lr-tb;text-anchor:start\" >${1}</text> </svg>$" \
 /tmp/barcode.svg > /tmp/label.svg
 
 # convert to pdf
